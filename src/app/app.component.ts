560db1c10bd07d893d18e473ee0e836d362b937f
Beatriz <beatriz.omori@edu.unipar.br>
2024-11-13 21:12:40 -0300

    criado header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CatalogoComponent} from './catalogo/catalogo.component';
+import {HeaderComponent} from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CatalogoComponent],
+  imports: [RouterOutlet, CatalogoComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
